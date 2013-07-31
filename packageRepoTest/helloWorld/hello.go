92159420aeb0996a3b638920cc748eecf815fc9d
Mark Bowser <markbowser9@gmail.com>
2013-07-30 20:10:56 -0400

    removed test files. we're workin on a server now :)

--- packageRepoTest/helloWorld/hello.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-import "packageRepoTest/newMath"
-
-func main() {
-  fmt.Printf("Hello, world. Sqrt(2) = %v\n", newmath.Sqrt(2))
-}
-
--- packageRepoTest/newMath/sqrt.go
+++
@@ -1,9 +0,0 @@
-package newmath
-
-func Sqrt(x float64) float64 {
-    z := 1.0
-    for i := 0; i < 1000; i++ {
-        z -= (z*z - x) / (2 * z)
-    }
-    return z
-}
